946e862b9eeb85e07542fbc502ecf7a107835834
Matt Ranlett <matt.ranlett@3ci.tech>
2026-02-04 20:11:34 -0500

    Initial Commit

---
+++ ViLM/ViLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViLMApp.swift
+//  ViLM
+//
+//  Created by Matt Ranlett on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ViLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
